2e37b959ca1a616b126d6ccc5dbf590991d83917
lmargain <787105@seq.org>
2016-09-08 08:27:15 -0700

    Add files via upload

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -5,7 +5,27 @@
  */
 public class BallRunner
 {
-  public static void run (){
-      BallWorld ballWorld = new BallWorld(50, 50);
+    BallWorld ballWorld;
+    TGPoint entrancePoint;
+    BallBot[] ballBotArray;
+   public BallRunner(BallWorld ballWorld, TGPoint entrancePoint, int ballBotArrayLength){
+       this.ballWorld = ballWorld;
+       this.entrancePoint = entrancePoint;
+       ballBotArray = new BallBot[ballBotArrayLength];
     }
+   public int findFreeBallBotIndex(){
+        for(int i = 0; i < ballBotArray.length; i++){
+            if(ballBotArray[i]==null)return i;
+        }
+        return ballBotArray.length;
+    }
+   public static void activity1 (){
+       BallWorld ballWorld = new BallWorld (200, 200);
+       TGPoint tgPoint = new TGPoint (0,0);
+       BallBot ballBot = new BallBot (ballWorld, tgPoint, 90.0, 30);
+       while(true){
+           if(ballBot.canMoveForward(ballWorld)) ballBot.moveForward();
+           else ballBot.setHeading((ballBot.getHeading()+90)%360);
+        }
+     }
 }
